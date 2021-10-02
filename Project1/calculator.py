8c08542c37b35f32bbf5f6175a02d3806eb93f50
Sheel <patel7sa@mail.uc.edu>
2021-10-01 22:58:53 -0400

    cleaned some code

--- Project1/calculator.py
+++ Project1/calculator.py
@@ -24,29 +24,26 @@ def div(x, y):
     return (x/y)
 
 
-user_input = int(input("Select operation: 1,2,3,4: "))
+print("Choose form the following functions: ")
+print("1 for Addition")
+print("2 for Subtraction")
+print("3 for Multiplication")
+print("4 for Division")
+
 time.sleep(2)
+
+user_input = int(input("Select operation: 1,2,3,4: "))
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 if user_input == 1:
-    print(add(num1, num2))
+    print(num1 + " + " + num2 + " = " + add(num1, num2))
 elif user_input == 2:
-    print(sub(num1, num2))
+    print(num1 + " - " + num2 + " = " + sub(num1, num2))
 elif user_input == 3:
-    print(mul(num1, num2))
+    print(num1 + " * " + num2 + " = " + mul(num1, num2))
 elif user_input == 4:
-    print(div(num1, num2))
+    print(num1 + " / " + num2 + " = " + div(num1, num2))
 else:
     print("Invalid input. Try again!")
-
-
-def again():
-    return
-
-
-# again_input = input("Calculate again? : Y or N \n")
-# if again_input == "y" or "Y":
-#     calculator()
-# else:
-#     exit()
