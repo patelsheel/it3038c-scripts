262f1bf95cd60fc6ad5ed3ac2754e9f2e2b37173
patelsheel <patel7sa@mail.uc.edu>
2021-10-28 00:01:10 -0400

    Adding Lab 07 files

---
+++ Lab07/Counters.ps1
@@ -0,0 +1,17 @@
+﻿$Machines = 'patel7sa-win'
+$LogFile = "C:\logs\cpu.log"
+Clear-Content($LogFile)
+
+Foreach($machine in $Machines)
+{
+    $RCounters = Get-Counter -ListSet * -ComputerName $machine
+    "There are {0} counters on {1}" -f $RCounters.count, ($machine)
+
+    $pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 10).CounterSamples.CookedValue
+    $sample = 1
+    foreach ($p in $pt){
+        $date = Get-date -Format g
+        "{3} - Sample {0}: CPU is at {1}% on {2} " -f [int]$sample, [int]$p, $machine, $date | out-file -Append $LogFile
+        $sample++
+    } 
+}
---
+++ Lab07/rando.ps1
@@ -0,0 +1,10 @@
+﻿$Rando = 0
+
+$LogFile = "C:\logs\rando.log"
+clear-content $LogFile
+
+for($i=0; $i -lt 5; $i++){
+    $Rando = Get-Random -Maximum 1000 -Minimum 1
+    Write-Host($Rando)
+    Add-Content $LogFile "INFO: Random Number is ${RANDO}"
+}
