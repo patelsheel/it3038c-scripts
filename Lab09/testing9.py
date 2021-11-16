4b3b07e17344cd65f15d1f5c4ed13c1e3a673716
Sheel <patel7sa@mail.uc.edu>
2021-11-16 10:20:38 -0500

    Adding Lab09 files

--- Lab09/testing9.py
+++ Lab09/testing9.py
@@ -5,3 +5,8 @@ r = requests.get("http://localhost:3000")
 data = r.json()
 
 print(data)
+
+i = 0
+while i < len(data):
+    i = i + 1
+    print(data[i - 1]['name'] + ' is ' + data[i - 1]['color'])
--- Lab09/weatherapi.py
+++ Lab09/weatherapi.py
@@ -9,5 +9,5 @@ r = requests.get(
     'https://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=74f68dae8e0afb05e1a44c5b86cc8ae3' % zip)
 data = r.json()
 
-print(type(data['weather'][0]['description']))
+# print(type(data['weather'][0]['description']))
 print('The weather in %s is %s' % (zip, data['weather'][0]['description']))
